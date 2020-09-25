b1e6f8a4d2e54487523e2a87c5ab5411dd8c0489
Chetan Jain <JAIN.CJ.CHETAN@gmail.com>
2020-09-25 12:03:22 +0530

    Updates

---
+++ precache-manifest.1bd4ebd6c927fa47c33d4b308628675c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b5868e06938404722c3ab55d8246923",
+    "url": "/NewsLaundry-Chetan/index.html"
+  },
+  {
+    "revision": "d43cf1364da46f62be21",
+    "url": "/NewsLaundry-Chetan/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1b32a974f2fb03f2cd1c",
+    "url": "/NewsLaundry-Chetan/static/css/main.d71b82ff.chunk.css"
+  },
+  {
+    "revision": "d43cf1364da46f62be21",
+    "url": "/NewsLaundry-Chetan/static/js/2.93e0af6f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/NewsLaundry-Chetan/static/js/2.93e0af6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b32a974f2fb03f2cd1c",
+    "url": "/NewsLaundry-Chetan/static/js/main.ff6d8386.chunk.js"
+  },
+  {
+    "revision": "37926c7e3d7dfe775267",
+    "url": "/NewsLaundry-Chetan/static/js/runtime-main.78004d4e.js"
+  }
+]);
